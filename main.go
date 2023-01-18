4dbf922fbb10676988812f7b16fc38debbbacc9f
osawa-koki <github@osawa.dev>
2023-01-19 01:52:59 +0900

    簡単なコード書いた。

--- main.go
+++ main.go
@@ -51,13 +51,14 @@ func HelloWorld(w http.ResponseWriter, r *http.Request) {
 		cy = 0.156
 	}
 
+	c := complex(cx, cy)
+
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for py := 0; py < height; py++ {
 		y := float64(py)/float64(height)*(ymax-ymin) + ymin
 		for px := 0; px < width; px++ {
 			x := float64(px)/float64(width)*(xmax-xmin) + xmin
 			z := complex(x, y)
-			c := complex(cx, cy)
 			img.Set(px, py, julia(z, c))
 		}
 	}
