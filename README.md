9addcf7b45d91a17f99aece210d25302053299f7
osawa-koki <github@osawa.dev>
2023-01-19 01:43:24 +0900

    fst commit;;

---
+++ README.md
@@ -0,0 +1,10 @@
+# julia-by-cloudfunc
+
+🍒🍒🍒 `Cloud Functions`でジュリア集合を描写するAPIを提供してみる。  
+AWSのlambdaやAzureのFunctionsと比較すると少し開発しにくいかも、、、  
+
+![成果物](./.development/img/julia.png)  
+
+実際にパラメタを指定してAPIを呼んでいるGIF画像も紹介。  
+
+![成果物](./.development/img/fruit.gif)  
---
+++ main.go
@@ -0,0 +1,81 @@
+package p
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math/cmplx"
+	"net/http"
+	"strconv"
+)
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+
+	xmin, err := strconv.ParseFloat(r.FormValue("xmin"), 64)
+	if err != nil {
+		xmin = -2
+	}
+
+	ymin, err := strconv.ParseFloat(r.FormValue("ymin"), 64)
+	if err != nil {
+		ymin = -2
+	}
+
+	xmax, err := strconv.ParseFloat(r.FormValue("xmax"), 64)
+	if err != nil {
+		xmax = 2
+	}
+
+	ymax, err := strconv.ParseFloat(r.FormValue("ymax"), 64)
+	if err != nil {
+		ymax = 2
+	}
+
+	width, err := strconv.Atoi(r.FormValue("width"))
+	if err != nil {
+		width = 1024
+	}
+
+	height, err := strconv.Atoi(r.FormValue("height"))
+	if err != nil {
+		height = 1024
+	}
+
+	cx, err := strconv.ParseFloat(r.FormValue("cx"), 64)
+	if err != nil {
+		cx = -0.8
+	}
+
+	cy, err := strconv.ParseFloat(r.FormValue("cy"), 64)
+	if err != nil {
+		cy = 0.156
+	}
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/float64(height)*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/float64(width)*(xmax-xmin) + xmin
+			z := complex(x, y)
+			c := complex(cx, cy)
+			img.Set(px, py, julia(z, c))
+		}
+	}
+
+	w.Header().Set("Content-Type", "image/png")
+	png.Encode(w, img)
+}
+
+func julia(z, c complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + c
+		if cmplx.Abs(v) > 2 {
+			return color.Gray{255 - contrast*n}
+		}
+	}
+	return color.Black
+}
